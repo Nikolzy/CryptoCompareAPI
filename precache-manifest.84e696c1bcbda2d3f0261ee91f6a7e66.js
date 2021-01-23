e17df0837a0d0eec898d3eea3ef007c8cd22a8bf
Nikolay <koliahndeko@gmail.com>
2021-01-23 11:26:53 +0200

    default preset

---
+++ precache-manifest.84e696c1bcbda2d3f0261ee91f6a7e66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6181af5088ef74a0cab",
+    "url": "/CryptoCompareAPI/css/app.d0ed7386.css"
+  },
+  {
+    "revision": "a75390c196a111219fcb5b7c4b5165da",
+    "url": "/CryptoCompareAPI/index.html"
+  },
+  {
+    "revision": "a6181af5088ef74a0cab",
+    "url": "/CryptoCompareAPI/js/app.abc04906.js"
+  },
+  {
+    "revision": "9112aa2246b603a6cc6a",
+    "url": "/CryptoCompareAPI/js/chunk-vendors.c7eed0c4.js"
+  },
+  {
+    "revision": "cac439e5627fd04e389de3e7dd18fecb",
+    "url": "/CryptoCompareAPI/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/CryptoCompareAPI/robots.txt"
+  }
+]);
